888b1f192d8ea5bf8bebb177b06be1dace86eb49
Yovko Garkov <iovko1993@gmail.com>
2025-12-20 00:07:29 +0200

    Add navigation buttons in MyCars

--- MyCarInfo/wwwroot/js/main.js
+++ MyCarInfo/wwwroot/js/main.js
@@ -42,26 +42,20 @@ window.addEventListener('scroll', scrollHeader)
 const btns = document.querySelectorAll(".slider__bg-navBtn");
 const slides = document.querySelectorAll(".video__slide");
 
-var sliderNav = function(manual){
-    btns.forEach((btn) => {
-        btn.classList.remove("active");
+if (btns.length && slides.length) {
+    const sliderNav = (manual) => {
+        btns.forEach((btn) => btn.classList.remove("active"));
+        slides.forEach((slide) => slide.classList.remove("active"));
+
+        btns[manual].classList.add("active");
+        slides[manual].classList.add("active");
+    };
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            sliderNav(i);
+        });
     });
-
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
-
-
-    btns[manual].classList.add("active");
-    slides[manual].classList.add("active");
-}
-
-btns.forEach((btn, i) =>{
-    btn.addEventListener("click", () => {
-        sliderNav(i);
-    });
-});
-
 
 /*=============== POPULAR SWIPER ===============*/
 
